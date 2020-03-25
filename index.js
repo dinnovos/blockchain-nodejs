464425809be3e05a69937528102cc479648aec99
Jorge Gaitan <jorgedeveloper24@gmail.com>
2020-03-25 00:58:31 -0500

    Primer commit

---
+++ index.js
@@ -0,0 +1,16 @@
+import PKG from './package.json';
+import Block from './src/blockchain/block';
+
+const { name, version } = PKG;
+
+console.log(`${name} v${version}`);
+
+const { genesis } = Block;
+
+const block = Block.mine(genesis, 'd4t4-1');
+
+console.log(block.toString());
+
+const block2 = Block.mine(block, 'd4t4-2');
+
+console.log(block2.toString());
