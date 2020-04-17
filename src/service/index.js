ad3a3cda0908789c0dc420567574e998e9fafaea
Jorge Gaitan <jorgedeveloper24@gmail.com>
2020-04-16 21:29:12 -0500

    Endpoint para obtener el listado de bloques por http

---
+++ src/service/index.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+import Blockchain from '../blockchain';
+
+const { HTTP_PORT = 3000 } = process.env;
+
+const app = express();
+const blockchain = new Blockchain();
+blockchain.addBlock("data 2");
+
+app.use(bodyParser.json());
+
+app.get("/blocks", (req, res) => {
+	res.json(blockchain.blocks);
+});
+
+app.listen(HTTP_PORT, () => {
+  console.log(`Service HTTP:${HTTP_PORT} listening...`);
+});
