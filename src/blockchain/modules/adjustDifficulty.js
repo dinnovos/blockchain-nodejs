deeee7d4696cea8327294af6365956fc422b825f
Jorge Gaitan <jorgedeveloper24@gmail.com>
2020-04-27 08:41:42 -0500

    Bug fixed: src/blockchain/modules/adjustDifficulty.js estaba devolviendo valores negativos. Ahora el minimo de dificultad es de "0"

--- src/blockchain/modules/adjustDifficulty.js
+++ src/blockchain/modules/adjustDifficulty.js
@@ -3,5 +3,11 @@ const MINE_REATE = 3000;
 export default (previousBlock, timestamp) => {
 	const { difficulty } = previousBlock;
 
-	return previousBlock.timestamp + MINE_REATE > timestamp ? difficulty + 1 : difficulty - 1;
+	const difficultyFinally =  previousBlock.timestamp + MINE_REATE > timestamp ? difficulty + 1 : difficulty - 1;
+
+	if(difficultyFinally < 0){
+		return 0;
+	}
+
+	return difficultyFinally;
 }
